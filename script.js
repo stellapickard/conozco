d4779696a04e17d0bb47a0b9b9254f007bc303a8
Michael Venter <myv4me@virginia.edu>
2016-06-30 12:00:33 +0200

    Angular Linking - Buggy

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // NON-ANGULAR CODE FOR PROFILE PAGE IMAGE SWAP OUT, PHOTO UPLOAD AND FULL PAGE BACKGROUND 
 
 $(document).ready(function(){
-    console.log( "ready!" );
+	console.log( "ready!" );
 
 // WEB PAGE AUTO SIZE FUNCTION
 // function autoResizeDiv()
@@ -13,17 +13,17 @@ $(document).ready(function(){
 
 // PROFILE PAGE SWAP OUT FUNCTION
 
-    $('.default_display').mouseover(function(){
-            $(this).find('.hover_display').css('display','block');
-    });
+$('.default_display').mouseover(function(){
+	$(this).find('.hover_display').css('display','block');
+});
 
-    $('.default_display').mouseout(function(){
-        $(this).find('.hover_display').css('display','none');
-
+$('.default_display').mouseout(function(){
+	$(this).find('.hover_display').css('display','none');
+});
 // NON-ANGULAR CODE FOR PROFILE PAGE IMAGE SWAP OUT, PHOTO UPLOAD AND FULL PAGE BACKGROUND
 
 $(document).ready(function(){
-   console.log( "ready!" );
+	console.log( "ready!" );
 
 // WEB PAGE AUTO SIZE FUNCTION
 // function autoResizeDiv()
@@ -35,23 +35,23 @@ $(document).ready(function(){
 
 // PROFILE PAGE SWAP OUT FUNCTION
 
-   $('.default_display').mouseover(function(){
-           $(this).find('.hover_display').css('display','block');
-   });
+$('.default_display').mouseover(function(){
+	$(this).find('.hover_display').css('display','block');
+});
 
-   $('.default_display').mouseout(function(){
-       $(this).find('.hover_display').css('display','none');
-   });
+$('.default_display').mouseout(function(){
+	$(this).find('.hover_display').css('display','none');
+});
 
 
 // ON CLICK FUNCTION FOR MOBILE DESIGN
-   $('.default_display').click(function(){
-           if ($(this).find('.hover_display').css('display') === "none"){
-               $(this).find('.hover_display').css('display', 'block')
-           } else {
-               $(this).find('.hover_display').css('display', 'none');
-           }
-   });
+$('.default_display').click(function(){
+	if ($(this).find('.hover_display').css('display') === "none"){
+		$(this).find('.hover_display').css('display', 'block')
+	} else {
+		$(this).find('.hover_display').css('display', 'none');
+	}
+});
 
 // closing tag of on ready function
 });
@@ -59,30 +59,30 @@ $(document).ready(function(){
 // IMAGE UPLOAD FUNCTION
 
 function imgUploadFunction(){
-   var x = document.getElementById("photo_upload");
-   var txt = "";
-   if ('files' in x) {
-       if (x.files.length == 0) {
-           txt = "";
-       } else {
-           for (var i = 0; i < x.files.length; i++) {
-               txt += "<br><strong>" + (i+1) + " file</strong><br>";
-               var file = x.files[i];
-               if ('name' in file) {
-                   txt += "Name: " + file.name + "<br>";
-               }
-               if ('size' in file) {
-                   txt += "Size: " + file.size + " bytes <br>";
-               }
-           }
-       }
-   }
-   else {
-       if (x.value == "") {
-           txt += "Please select an image";
-       } else {
-           txt += "The files property is not supported by your browser!";
-           txt  += "<br>The path of the selected file: " + x.value; 
+	var x = document.getElementById("photo_upload");
+	var txt = "";
+	if ('files' in x) {
+		if (x.files.length == 0) {
+			txt = "";
+		} else {
+			for (var i = 0; i < x.files.length; i++) {
+				txt += "<br><strong>" + (i+1) + " file</strong><br>";
+				var file = x.files[i];
+				if ('name' in file) {
+					txt += "Name: " + file.name + "<br>";
+				}
+				if ('size' in file) {
+					txt += "Size: " + file.size + " bytes <br>";
+				}
+			}
+		}
+	}
+	else {
+		if (x.value == "") {
+			txt += "Please select an image";
+		} else {
+			txt += "The files property is not supported by your browser!";
+			txt  += "<br>The path of the selected file: " + x.value; 
            // If the browser does not support the files property, it will return the path of the selected file instead.
        }
    }
@@ -90,44 +90,45 @@ function imgUploadFunction(){
 }
 
 // END OF NON-ANGULAR CODE
+});
 
 var app = angular.module("ConozcoApp", ["ngRoute"]);
 
 // ROUTE CONFIGURATION
 
 app.config(function($routeProvider){
-   $routeProvider
 
-   .when ("/", {
-       templateUrl: "/login.html",
-       controller: "login_controller"
-   })
 
-   .when("/profile", {
-       templateUrl: "template/profile.html",
-       controller: "profile_controller"
-   })
+	$routeProvider.when ("/", {
+		templateUrl: "templates/login.html",
+		controller: "login_controller"
+	})
 
-   .when("/workfeed", {
-       templateUrl: "template/feed_work.html",
-       controller: "workfeed_controller"
-   })
+	$routeProvider.when("/profile", {
+		templateUrl: "templates/profile.html",
+		controller: "profile_controller"
+	})
 
-   .when("/generalfeed", {
-       tempalteUrl: "template/feed_general.html",
-       controller: "generalfeed_controller"
-   })
+	$routeProvider.when("/work", {
+		templateUrl: "templates/feed_work.html",
+		controller: "workfeed_controller"
+	})
 
-   .when("/admin", {
-       templateUrl: "template/admin.html",
-       controller: "admin_controller"
-   })
+	$routeProvider.when("/general", {
+		tempalteUrl: "templates/feed_general.html",
+		controller: "generalfeed_controller"
+	})
+
+	$routeProvider.when("/admin", {
+		templateUrl: "templates/admin.html",
+		controller: "admin_controller"
+	})
 
 });
 
 // LOGIN_CONTROLLER
 
-ConozcoApp.controller('login_controller', function($scope, $http){
+app.controller('login_controller', function($scope, $http){
 
 
 
@@ -136,7 +137,7 @@ ConozcoApp.controller('login_controller', function($scope, $http){
 
 // PROFILE_CONTROLLER
 
-ConozcoApp.controller('profile_controller', function($scope, $http){
+app.controller('profile_controller', function($scope, $http){
 
 
 
@@ -145,7 +146,7 @@ ConozcoApp.controller('profile_controller', function($scope, $http){
 
 // WORKFEED_CONTROLLER
 
-ConozcoApp.controller('workfeed_controller', function($scope, $http){
+app.controller('workfeed_controller', function($scope, $http){
 
 
 
@@ -154,7 +155,7 @@ ConozcoApp.controller('workfeed_controller', function($scope, $http){
 
 // GENERALFEED_CONTROLLER
 
-ConozcoApp.controller('generalfeed_controller', function($scope, $http){
+app.controller('generalfeed_controller', function($scope, $http){
 
 
 
@@ -163,7 +164,7 @@ ConozcoApp.controller('generalfeed_controller', function($scope, $http){
 
 // ADMIN_CONTROLLER
 
-ConozcoApp.controller('admin_controller', function($scope, $http){
+app.controller('admin_controller', function($scope, $http){
 
 
 
